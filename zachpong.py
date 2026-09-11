3432dddce613c1f0bd2cf80ed9ff1e743278b7a3
EkanshKhaitan <khaitanekansh1@gmail.com>
2026-09-11 21:17:14 +0530

    set caption to Zach Poing and Fixed ball getting stuck in paddle

--- zachpong.py
+++ zachpong.py
@@ -9,6 +9,7 @@ pygame.font.init()
 WIDTH = 1280
 HEIGHT = 720
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Zach Pong")
 clock = pygame.time.Clock()
 running = True
 dt = 0
@@ -20,7 +21,6 @@ ball_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 left_paddle = pygame.Rect(WIDTH // 5 , HEIGHT // 2, 20 , 200)
 
 zach = pygame.image.load('zach.png').convert_alpha()
-# zach = pygame.transform.scale(zach, (200, 200))
 zach = pygame.transform.smoothscale(zach, (200, 200))
 
 bone = pygame.mixer.Sound('bone.mp3')
@@ -28,6 +28,8 @@ coin = pygame.mixer.Sound('coin.mp3')
 score = 0
 font = pygame.font.Font('Minecraft-Seven_v2.woff2', 50)
 big_font = pygame.font.Font('Minecraft-Seven_v2.woff2', 200)
+
+last_frame_score = False
 
 while running:
     # poll for events
@@ -61,10 +63,14 @@ while running:
         vy *= -1
         bone.play()
 
-    if ball_rect.colliderect(left_paddle):
+    if ball_rect.colliderect(left_paddle) and not last_frame_score:
         vx *= -1
         coin.play()
         score += 1
+        last_frame_score = True
+    
+    if not ball_rect.colliderect(left_paddle):
+        last_frame_score = False
 
     if ball_pos.x <= 200:
         vx = 0
@@ -86,12 +92,12 @@ while running:
     if left_paddle.y > HEIGHT - 200:
         left_paddle.y = HEIGHT - 200
 
-
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         left_paddle.y -= 300 * dt
     if keys[pygame.K_s]:
         left_paddle.y += 300 * dt
+
 
 
     # flip() the display to put your work on screen
